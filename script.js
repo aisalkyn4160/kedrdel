cf92718e96dfe7710305329ac2e33bbc2ebecf11
aisalkyn4160 <aisalkyn.4160@gmail.com>
2026-06-03 10:32:24 +0700

    add animation to banner on mobile version

--- script.js
+++ script.js
@@ -6,88 +6,59 @@ const fgLayer2 = document.querySelector('.banner-bottom-img');
 
 const isMobile = window.innerWidth <= 767 || 'ontouchstart' in window;
 
-const MAX_MOVE_X = isMobile ? 16 : 60;
-const MAX_MOVE_Y = isMobile ? 20 : 100;
-
-let rafId = null;
-let isDragging = false; 
-
-function getCursorPosition(e, rect) {
-    if (e.touches) {
-        const touch = e.touches[0];
-        return {
-            x: (touch.clientX - rect.left) / rect.width,
-            y: (touch.clientY - rect.top) / rect.height
-        };
-    } else {
-        return {
-            x: (e.clientX - rect.left) / rect.width,
-            y: (e.clientY - rect.top) / rect.height
-        };
-    }
-}
-
-function handleMove(x, y) {
-    const moveX = (x - 0.5) * MAX_MOVE_X * 2;
-    const moveY = (y - 0.5) * MAX_MOVE_Y * 2;
-    
-    const bgXFactor = isMobile ? 0.5 : 0.3;
-    const bgYFactor = isMobile ? 0.5 : 0.2;
-    
-    bgLayer.style.transform = `translate(${moveX * bgXFactor}px, ${moveY * bgYFactor}px)`;
-    fgLayer.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    fgLayer2.style.transform = `translate(${moveX}px, ${moveY}px)`;
-}
-
-function resetPosition() {
-    if (rafId) cancelAnimationFrame(rafId);
-    bgLayer.style.transform = 'translate(0, 0)';
-    fgLayer.style.transform = 'translate(0, 0)';
-    fgLayer2.style.transform = 'translate(0, 0)';
-    isDragging = false;
-}
-
-banner.addEventListener('mousemove', (e) => {
-    if (isMobile) return; 
-    
-    if (rafId) cancelAnimationFrame(rafId);
-    rafId = requestAnimationFrame(() => {
-        const rect = banner.getBoundingClientRect();
-        const { x: mouseX, y: mouseY } = getCursorPosition(e, rect);
-        handleMove(mouseX, mouseY);
-    });
-});
-
-banner.addEventListener('mouseleave', resetPosition);
-
 if (isMobile) {
-    banner.addEventListener('touchstart', (e) => {
-        isDragging = true;
-    });
+    const animX = {
+        min: -16,    
+        max: 16,     
+        duration: 4000,
+        easing: 'ease-in-out'
+    };
     
-    banner.addEventListener('touchmove', (e) => {
-        if (!isDragging) return;
-        
-        if (Math.abs(e.touches[0].clientY - e.touches[0].clientY) > 30) {
-            return; 
-        }
-        
-        e.preventDefault(); 
-        
+    const animY = {
+        min: -12,
+        max: 12,
+        duration: 5000,
+        easing: 'ease-in-out'
+    };
+    
+    fgLayer.style.animation = `windFloatY ${animX.duration}ms ${animX.easing} infinite alternate`;
+    fgLayer2.style.animation = `windFloatY ${animY.duration}ms ${animY.easing} infinite alternate`;
+    bgLayer.style.animation = `windFloatY ${6000}ms ease-in-out infinite alternate`;
+    
+    fgLayer2.style.animationDelay = '0.5s';
+    
+    fgLayer.style.transform = '';
+    fgLayer2.style.transform = '';
+    bgLayer.style.transform = '';
+    
+} else {
+    const MAX_MOVE_X = 60;   
+    const MAX_MOVE_Y = 100; 
+    let rafId = null;
+
+    banner.addEventListener('mousemove', (e) => {
         if (rafId) cancelAnimationFrame(rafId);
         rafId = requestAnimationFrame(() => {
             const rect = banner.getBoundingClientRect();
-            const { x: touchX, y: touchY } = getCursorPosition(e, rect);
-            handleMove(touchX, touchY);
+            const mouseX = (e.clientX - rect.left) / rect.width;
+            const mouseY = (e.clientY - rect.top) / rect.height;
+            const moveX = (mouseX - 0.5) * MAX_MOVE_X * 2;
+            const moveY = (mouseY - 0.5) * MAX_MOVE_Y * 2; 
+  
+            bgLayer.style.transform = `translate(${moveX * 0.3}px, ${moveY * 0.2}px)`;
+            fgLayer.style.transform = `translate(${moveX}px, ${moveY}px)`;
+            fgLayer2.style.transform = `translate(${moveX}px, ${moveY}px)`;
         });
     });
-    
-    banner.addEventListener('touchend', () => {
-        resetPosition();
+
+    banner.addEventListener('mouseleave', () => {
+        if (rafId) cancelAnimationFrame(rafId);
+        bgLayer.style.transform = 'translate(0, 0)';
+        fgLayer.style.transform = 'translate(0, 0)';
+        fgLayer2.style.transform = 'translate(0, 0)';
     });
-    
-    banner.addEventListener('touchcancel', resetPosition);
 }
+
 // ------------------------------------------------ About-swiper------------------------------------------------
     const aboutSwiper = new Swiper('.about-swiper .swiper', {
       slidesPerView: 'auto',
